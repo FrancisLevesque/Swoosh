82c65eff4148b1bfb1650219ba3fc95d2fb21636
Francis Levesque <flevesqu@cisco.com>
2020-03-15 15:22:29 +0100

    Passing values from league activity to skill

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -4,16 +4,47 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
+    fun onMensClicked(view: View) {
+        womensLeagueButton.isChecked = false
+        coedLeagueButton.isChecked = false
+
+        selectedLeague = "mens"
+    }
+
+    fun onWomensClicked(view: View) {
+        mensLeagueButton.isChecked = false
+        coedLeagueButton.isChecked = false
+
+        selectedLeague = "womens"
+    }
+
+    fun onCoedClicked(view: View) {
+        mensLeagueButton.isChecked = false
+        womensLeagueButton.isChecked = false
+
+        selectedLeague = "co-ed"
+    }
+
     fun leagueNextClicked(view: View) {
-        val skillIntent = Intent(this, SkillActivity::class.java)
-        startActivity(skillIntent)
+        if (selectedLeague != "") {
+            val skillIntent = Intent(this, SkillActivity::class.java)
+            skillIntent.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skillIntent)
+        } else {
+            Toast.makeText(this, "Please select a League.", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
